be21582b1b364465c7bb6d537e673a8d1f415437
Helal1001 <63949422+Mohamed-S-Helal@users.noreply.github.com>
2021-12-23 08:01:04 -0800

    command line args

--- rebaseAPI.py
+++ rebaseAPI.py
@@ -1,14 +1,19 @@
 import requests
 import zipfile
 import io
+import sys
 
-def csv2sqlite(csv, output):
+def csv2sqlite(input, output):
     url = 'https://www.rebasedata.com/api/v1/convert?outputFormat=sqlite'
     files = {'files[]': open('country_info.csv', 'r')}
     r = requests.post(url, files=files)
 
     with zipfile.ZipFile(io.BytesIO(r.content)) as zip_ref:
-        zip_ref.extractall('./')
+        zip_ref.extractall(output)
 
 if __name__ == '__main__':
-    csv2sqlite('country_info.csv', './')
+    args = sys.argv
+    if args[1]:
+        input = args[1]
+        output = args[2] if len(args)==3 else './'
+        csv2sqlite(input, output)
