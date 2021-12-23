fc6ce3a88cf9530043f0cc864084f1dc7c5f014f
Helal1001 <63949422+Mohamed-S-Helal@users.noreply.github.com>
2021-12-23 08:12:18 -0800

    Update rebaseAPI.py

--- rebaseAPI.py
+++ rebaseAPI.py
@@ -5,7 +5,7 @@ import sys
 
 def csv2sqlite(input, output):
     url = 'https://www.rebasedata.com/api/v1/convert?outputFormat=sqlite'
-    files = {'files[]': open('country_info.csv', 'r')}
+    files = {'files[]': open(input, 'r')}
     r = requests.post(url, files=files)
 
     with zipfile.ZipFile(io.BytesIO(r.content)) as zip_ref:
@@ -13,7 +13,10 @@ def csv2sqlite(input, output):
 
 if __name__ == '__main__':
     args = sys.argv
-    if args[1]:
+    ## test on country_info.csv file
+    # csv2sqlite('country_info.csv', './')
+    if len(args)>1:
         input = args[1]
         output = args[2] if len(args)==3 else './'
         csv2sqlite(input, output)
+        
